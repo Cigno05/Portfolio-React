3fbc79a2b1a472928b165f0c111eeeb2cde24bb1
Cigno05 <paolocalcagni.82@gmail.com>
2024-07-31 18:39:27 +0200

    ADD card prototype

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,11 +9,18 @@ function Main() {
                 
                 <Hero></Hero>
 
-                <div className="container">
+                <div className="container mt-4 ">
                     <div className="row">
-                        <div className="col">
-                            <Card></Card>
-
+                        <h1>I miei progetti</h1>
+                    </div>
+                    <div className="row">
+                        <div className="col-12 col-lg-6">
+                            <Card
+                            title='fooder'
+                            imgUrl='/img/vite.svg'
+                            description='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ducimus illum cumque obcaecati id consectetur.'
+                            technology='react, css. bootstrap'
+                            ></Card>
                         </div>
                     </div>
                 </div>
--- src/components/partials/Card.jsx
+++ src/components/partials/Card.jsx
@@ -1,13 +1,24 @@
 
 
-function Card() {
+function Card({title, imgUrl, description, technology}) {
+    
 
     return (
         <>
-        <div id="card">
-            
-            <h1 className="my-card">Card</h1>
-        </div>
+            <div id="card" className="card">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src={imgUrl} class="img-fluid rounded-start" alt="..."></img>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">{title}</h5>
+                            <p class="card-text">{description}</p>
+                            <p class="card-text"><small class="text-body-secondary">{technology}</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
